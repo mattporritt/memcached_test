a597cb495b152d117fc62383c997695699a38cfe
Matt Porritt <mattp@catalyst-au.net>
2015-12-31 15:32:58 +1100

    fix hanging whitespace

--- memcached_test.php
+++ memcached_test.php
@@ -16,48 +16,48 @@ foreach ($itterations as $itteration){
     // setup  Memcached connection
     $memcached = new Memcached(crc32('test'));
     $memcached->addServer('localhost', 11211);
-    
+
     // Initialize values
     for ($i=0;$i<$itteration;$i++) {
         $values[sprintf('%020s',$i)]=sha1($i);
     }
-    
+
     // set values
     $start = microtime(true);
     foreach ($values as $key => $value){
         $memcached->set($key, $value);
     }
     $set_time = sprintf('%01.4f', microtime(true) - $start);
-    
+
     // get values (hits)
     $start = microtime(true);
     foreach ($values as $key => $value) {
         $memcached->get($key);
     }
     $get_time = sprintf('%01.4f', microtime(true) - $start);
-    
+
     // get values (miss)
     $start = microtime(true);
     foreach ($values as $key => $value) {
         $memcached->get('miss'.$key);
     }
     $miss_time = sprintf('%01.4f', microtime(true) - $start);
-    
+
     // Delete values
     $start = microtime(true);
     foreach ($values as $key => $value) {
         $memcached->delete($key);
     }
     $del_time = sprintf('%01.4f', microtime(true) - $start);
-    
+
     // Print table values
-    printf("[%0{$cellsize}s]|", $c);
+    printf("%' {$cellsize}s|", $itteration);
     printf("[%0{$cellsize}s]|", $set_time);
     printf("[%0{$cellsize}s]|", $get_time);
     printf("[%0{$cellsize}s]|", $miss_time);
     printf("[%0{$cellsize}s]|", $del_time);
     echo "\n";
-    
+
     // teardown Memcached connection
     $memcached->flush();
     $memcached->quit();
